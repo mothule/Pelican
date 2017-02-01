4187820943f2373fafc2f26c8aca56726694e44b
Motoki Kawakami <mothule.dev@gmail.com>
2017-02-01 10:46:44 +0900

    Add projects.

---
+++ Pelican/Pelican.h
@@ -0,0 +1,19 @@
+//
+//  Pelican.h
+//  Pelican
+//
+//  Created by mothule on 2017/02/01.
+//  Copyright © 2017年 mothule. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Pelican.
+FOUNDATION_EXPORT double PelicanVersionNumber;
+
+//! Project version string for Pelican.
+FOUNDATION_EXPORT const unsigned char PelicanVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pelican/PublicHeader.h>
+
+
